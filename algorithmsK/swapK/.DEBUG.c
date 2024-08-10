76f91d3ade2050134def465ad187379ded7204a9
Kikols <francisco.lapamsilva@gmail.com>
2024-08-10 11:52:29 +0100

    more test cases for "algorithmsK/swapK"

--- algorithmsK/swapK/.DEBUG.c
+++ algorithmsK/swapK/.DEBUG.c
@@ -11,9 +11,9 @@
 #define VERBOSE false
 
 int test_intswap(int n);
-//int test_doubleswap(int n);
-//int test_charswap(int n);
-//int test_binswap(int n);
+int test_doubleswap(int n);
+int test_charswap(int n);
+int test_binswap(int n, size_t datatype_size);
 
 
 int main() {
@@ -23,6 +23,18 @@ int main() {
     res = test_intswap(100);
     sum += res;
     printf("[MAIN-DEBUG] \"test_intswapK\": %d\n", res);
+
+    res = test_doubleswap(100);
+    sum += res;
+    printf("[MAIN-DEBUG] \"test_doubleswapK\": %d\n", res);
+
+    res = test_charswap(100);
+    sum += res;
+    printf("[MAIN-DEBUG] \"test_charswapK\": %d\n", res);
+
+    res = test_binswap(100, sizeof(int));
+    sum += res;
+    printf("[MAIN-DEBUG] \"test_binswapK\": %d\n", res);
 
     return sum;
 }
@@ -48,5 +60,74 @@ int test_intswap(int n) {
         }
     }
 
+    return 0;
+}
+
+int test_doubleswap(int n) {
+    for (int i=0; i<n; i++) {
+        double or_a = (double)rand() / (double)RAND_MAX;
+        double or_b = (double)rand() / (double)RAND_MAX;
+        double a = or_a;
+        double b = or_b;
+        doubleswapK(&a, &b, sizeof(double));
+        if (a==or_b && b==or_a) {
+            #if VERBOSE
+            printf("swapK test %ld passed: a->%f, b->%f\n", i, a, b);
+            #endif
+        } else {
+            #if VERBOSE
+            printf("swapK test %ld failed: a->%f, b->%f\n", i, a, b);
+            #endif
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int test_charswap(int n) {
+    for (int i=0; i<n; i++) {
+        char or_a = rand() % 256;
+        char or_b = rand() % 256;
+        char a = or_a;
+        char b = or_b;
+        charswapK(&a, &b, sizeof(char));
+        if (a==or_b && b==or_a) {
+            #if VERBOSE
+            printf("swapK test %ld passed: a->%c, b->%c\n", i, a, b);
+            #endif
+        } else {
+            #if VERBOSE
+            printf("swapK test %ld failed: a->%c, b->%c\n", i, a, b);
+            #endif
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int test_binswap(int n, size_t datatype_size) {
+    for (int i=0; i<n; i++) {
+        char or_a[datatype_size];
+        char or_b[datatype_size];
+        for (int j=0; j<datatype_size; j++) {
+            or_a[j] = rand() % 256;
+            or_b[j] = rand() % 256;
+        }
+        char a[datatype_size];
+        char b[datatype_size];
+        memcpy(a, or_a, datatype_size);
+        memcpy(b, or_b, datatype_size);
+        binswapK(a, b, datatype_size);
+        if (memcmp(a, or_b, datatype_size)==0 && memcmp(b, or_a, datatype_size)==0) {
+            #if VERBOSE
+            printf("swapK test %ld passed: a->%s, b->%s\n", i, a, b);
+            #endif
+        } else {
+            #if VERBOSE
+            printf("swapK test %ld failed: a->%s, b->%s\n", i, a, b);
+            #endif
+            return 1;
+        }
+    }
     return 0;
 }
