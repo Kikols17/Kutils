8b7888d7664a2f6d344af3baac29229c0175daed
Kikols <francisco.lapamsilva@gmail.com>
2024-09-15 17:23:39 +0100

    add macro_Kutils.h

---
+++ src/macro_Kutils.h
@@ -0,0 +1,10 @@
+#ifndef MACROS_KUTILS_H
+#define MACROS_KUTILS_H
+
+// This file is used to define all macros used in the project
+
+// Macro to identify the unused parameter in a function
+#define UNUSED_K(x) (void)(x)
+
+
+#endif
