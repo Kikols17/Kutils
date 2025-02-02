2280a3eb1102ae9c814b1a5534d12e80e6c7f3b5
Kikols <francisco.lapamsilva@gmail.com>
2025-02-02 18:44:07 +0000

    add valid tests to issorteduntilK

---
+++ tests/valid_tests/issorteduntilK_sorted.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "issorteduntilK.h"
+
+#define MY_COMPARE(a, b) ((a) > (b))
+DEFINE_ISSORTEDUNTILK(int, int, MY_COMPARE)
+
+
+int main(int argc, const char *argv[]) {
+    long long int n = atoll(argv[1]);
+    
+    int *array = (int*)malloc(n*sizeof(int));
+    for (long long int i=0; i<n; i++) {
+        array[i] = i;
+    }
+
+    printf("%ld\n", issorteduntilK_int(&array[0], &array[n])-&array[0]);
+
+    return 0;
+}
