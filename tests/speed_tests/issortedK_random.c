b9e9be9fb3de1efa1e47eeb9326cff45281afc63
Kikols <francisco.lapamsilva@gmail.com>
2025-02-02 02:39:29 +0000

    add speed tests to issortedK

---
+++ tests/speed_tests/issortedK_random.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "issortedK.h"
+
+#define MY_COMPARE(a, b) ((a) > (b))
+DEFINE_ISSORTEDK(int, int, MY_COMPARE)
+
+
+int main(int argc, const char *argv[]) {
+    long long int n = atoll(argv[1]);
+    srand(n);
+
+    int *array = (int*)malloc(n*sizeof(int));
+    for (long long int i=0; i<n; i++) {
+        array[i] = rand();
+    }
+
+    int res = issortedK_int(&array[0], &array[n]);
+
+    return 0;
+}
