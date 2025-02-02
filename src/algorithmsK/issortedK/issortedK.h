9c99f53b282114b9e9ee60adbc9e614a81c14502
Kikols <francisco.lapamsilva@gmail.com>
2025-02-02 02:38:55 +0000

    add documentation comments for issortedK

--- src/algorithmsK/issortedK/issortedK.h
+++ src/algorithmsK/issortedK/issortedK.h
@@ -5,6 +5,27 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+// This is the template for the "issortedK_<type>" function
+/*
+    DEFINE_ISSORTEDK(TYPE, TYPENAME, COMPARE)
+        -> TYPE             (type [ ex: int ])
+        -> TYPENAME         (name that created function will take [ ex: int -> issortedK_int(...) ])
+        -> COMPARE(a,b)     (compare expression [ ex: #define MY_COMPARE(a, b) ((a)>(b)) ])
+
+
+
+
+
+    bool issortedK_<type>(<type>* begin, <type>* end):
+        iteratively checks if a range that starts at "begin" and who's last element is the one before "end"
+        is sorted according to the COMPARE macro.
+        If you want to check if a range is sorted, simply use the same COMPARE that you would use to sort it.
+
+        -> <type> begin     (pointer to the first element of the array [ ex: array[0] ])
+        -> <type> end       (pointer to the first element after the array that does not belong [ex: array[n] ])
+
+ */
+
 #define DEFINE_ISSORTEDK(TYPE, TYPENAME, COMPARE) \
   \
 bool issortedK_##TYPENAME(TYPE* begin, TYPE* end) { \
