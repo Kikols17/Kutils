05477ce34ce3e5b1aea0ad9f63a5bc436b98c890
Kikols <francisco.lapamsilva@gmail.com>
2025-02-14 23:47:57 +0000

    create maxelementK and minelement template functions

---
+++ src/algorithmsK/minmaxK/minelementK.h
@@ -0,0 +1,33 @@
+#ifndef MINELEMENTK_H
+#define MINELEMENTK_H
+
+// This is the template macro for the "minelementK_<type>" function
+/*
+    DEFINE_MINELEMENTK(TYPE, TYPENAME)
+        -> TYPE             (type [ ex: int ])
+        -> TYPENAME         (name that created function will take [ ex: int -> minelementK_int(...) ])
+
+
+
+
+
+
+    TYPE minelementK_<type>(TYPE* begin, TYPE* end):
+        Find the minimum element in the range defined by "begin" and "end".
+        
+        -> TYPE begin     (pointer to the first element of the array [ ex: array[0] ])
+        -> TYPE end       (pointer to the first element after the array that does not belong [ex: array[n] ])
+        -> return         (the minimum element in the range)
+ */
+
+#define DEFINE_MINELEMENTK(TYPE, TYPENAME) \
+  \
+TYPE minelementK_##TYPENAME(TYPE* begin, TYPE* end) { \
+    TYPE min = *begin; \
+    while (++begin!=end) { \
+        min = min < *begin ? min : *begin; \
+    } \
+    return min; \
+}
+
+#endif
