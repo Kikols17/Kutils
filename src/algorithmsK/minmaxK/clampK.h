de769410934de761c04d792702f0ebb425734862
Kikols <francisco.lapamsilva@gmail.com>
2025-02-14 23:57:45 +0000

    add CLAMPMAXK and CLAMPMINK to clampK.h

--- src/algorithmsK/minmaxK/clampK.h
+++ src/algorithmsK/minmaxK/clampK.h
@@ -11,14 +11,14 @@
         -> max           (maximum value)
         -> return        (the value of "a" clamped between "min" and "max")
 
-    CLAMPK_MAX(a, max):
+    CLAMPMAXK(a, max):
         This macro returns the value of "a" clamped between "-infinity" and "max".
         This works for all types that support the "<" operator [ ex: int, float, double, char ]
         -> a             (value to clamp)
         -> max           (maximum value)
         -> return        (the value of "a" clamped between "-infinity" and "max")
 
-    CLAMPK_MIN(a, min):
+    CLAMPMINK(a, min):
         This macro returns the value of "a" clamped between "min" and "+infinity".
         This works for all types that support the ">" operator [ ex: int, float, double, char ]
         -> a             (value to clamp)
@@ -28,7 +28,7 @@
  */
 
 #define CLAMPK(a, min, max) ( (a<min) ? min:((a>max) ? (max:a)) )
-#define CLAMPK_MAX(a, max) ( (a>max) ? max:a )
-#define CLAMPK_MIN(a, min) ( (a<min) ? min:a )
+#define CLAMPMAXK(a, max) ( (a>max) ? max:a )
+#define CLAMPMINK(a, min) ( (a<min) ? min:a )
 
 #endif
