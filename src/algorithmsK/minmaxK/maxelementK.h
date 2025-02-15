e1c776bbb848ec10650f48625f09ada0b32cd566
Kikols <francisco.lapamsilva@gmail.com>
2025-02-15 02:26:31 +0000

    fix maxelementK template function

--- src/algorithmsK/minmaxK/maxelementK.h
+++ src/algorithmsK/minmaxK/maxelementK.h
@@ -12,20 +12,20 @@
 
 
 
-    TYPE maxelementK_<type>(TYPE* begin, TYPE* end):
-        Find the maximum element in the range defined by "begin" and "end".
+    TYPE* maxelementK_<type>(TYPE* begin, TYPE* end):
+        Find the pointer to the first maximum element in the range defined by "begin" and "end".
         
         -> TYPE begin     (pointer to the first element of the array [ ex: array[0] ])
         -> TYPE end       (pointer to the first element after the array that does not belong [ex: array[n] ])
-        -> return         (the maximum element in the range)
+        -> return         (pointer to the first maximum element in the range)
  */
 
 #define DEFINE_MAXELEMENTK(TYPE, TYPENAME) \
   \
-TYPE maxelementK_##TYPENAME(TYPE* begin, TYPE* end) { \
-    TYPE max = *begin; \
+TYPE* maxelementK_##TYPENAME(TYPE* begin, TYPE* end) { \
+    TYPE* max = begin; \
     while (++begin!=end) { \
-        max = max>*begin ? max:*begin; \
+        max = *max>*begin ? max:begin; \
     } \
     return max; \
 }
