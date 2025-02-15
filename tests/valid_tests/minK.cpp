04fbf548d4ded9e2906ec7f2693c41ee35eaecaf
Kikols <francisco.lapamsilva@gmail.com>
2025-02-15 01:00:16 +0000

    add valid/speed tests for MINK macro

---
+++ tests/valid_tests/minK.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    a = 0;
+    b = 1;
+    cout << min(a, b) << endl;
+
+    a = 5;
+    b = 3;
+    cout << min(a, b) << endl;
+
+    a = 7;
+    b = 7;
+    cout << min(a, b) << endl;
+
+    return 0;
+}
