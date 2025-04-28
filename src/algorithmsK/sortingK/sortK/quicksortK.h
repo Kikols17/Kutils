e1c61601b5f8843e250fb2a3627b71d604dfbaca
Kikols <francisco.lapamsilva@gmail.com>
2025-04-28 19:50:40 +0100

    make "quicksortK"'s heap stack be smaller, and fix aestetic rules

--- src/algorithmsK/sortingK/sortK/quicksortK.h
+++ src/algorithmsK/sortingK/sortK/quicksortK.h
@@ -9,28 +9,35 @@
 /*
     DEFINE_QUICKSORTK(TYPE, TYPENAME, COMPARE)
         -> TYPE             (type [ ex: int ])
-        -> TYPENAME         (name that created function will take [ ex: int -> quicksortK_int(...) ])
-        -> COMPARE(a,b)     (compare expression [ ex: #define MY_COMPARE(a, b) ((a)>(b)) ])
+        -> TYPENAME         (name that created function will take
+                             [ ex: int -> quicksortK_int(...) ])
+        -> COMPARE(a,b)     (compare expression
+                             [ ex: #define MY_COMPARE(a, b) ((a)>(b)) ])
 
 
 
 
 
     void quicksortK_<type>(<type>* begin, <type>* end):
-        Quicksorts the given array, that starts at "begin" and who's last element is the one before "end".
-        This implementation avoids recursive calls for speed. The todo stack is alloced in the heap, otherwise
-        the number of elements would be limited by the max size of the stack frame, which depends on OS.
-        The stack has size n (because each todo entry requires 2 parameters)
+        Quicksorts the given array, that starts at "begin" and who's last
+        element is the one before "end".
+        This implementation avoids recursive calls for speed. The todo stack is
+        alloced in the heap, otherwise the number of elements would be limited
+        by the max size of the stack frame, which depends on OS.
+        The stack has size n/2+1 (because each todo entry requires 2 parameters,
+        and +1 to prevent stack from ever being 1)
 
-        -> <type> begin     (pointer to the first element of the array [ ex: array[0] ])
-        -> <type> end       (pointer to the first element after the array that does not belong [ex: array[n] ])
+        -> <type> begin     (pointer to the first element of the array
+                             [ ex: array[0] ])
+        -> <type> end       (pointer to the first element after the array that
+                             does not belong [ex: array[n] ])
 
  */
 
 #define DEFINE_QUICKSORTK(TYPE, TYPENAME, COMPARE) \
     \
 void quicksortK_##TYPENAME(TYPE* begin, TYPE* end) { \
-    TYPE** stack = (TYPE**)malloc(((end-begin)*2)*sizeof(TYPE*)); \
+    TYPE** stack = (TYPE**)malloc(((end-begin)+2)*sizeof(TYPE*)); \
     TYPE** top = stack; \
     *top++ = begin; \
     *top++ = end; \
