a12bfc3def438a3225c331e2257aeecd117151d1
Kikols <francisco.lapamsilva@gmail.com>
2025-07-15 15:21:59 +0100

    "mergeK" first draft

---
+++ src/algorithmsK/otheropsK/mergeK.h
@@ -0,0 +1,55 @@
+#ifndef MERGEK_H
+#define MERGEK_H
+
+// This is the template macro for the "mergeK_<type>" function
+/*
+    DEFINE_MERGEK(TYPE, TYPENAME, COMPARE)
+        -> TYPE         (type [ ex: int ])
+        -> TYPENAME     (name that created function will take [ ex: int -> mergeK_int(...) ])
+        -> COMPARE      (comparison function [ ex: ( (a) < (b) ) ])
+
+
+
+
+
+
+    TYPE* mergeK_<type>(TYPE* begin1, TYPE* end1, TYPE* begin2, TYPE* end2, TYPE* output):
+        Merges two sorted arrays arrays (array1 and array2), into output array.
+        Make sure that output array's size is not less than both arrays', as it will result of Segmentation Fault.
+
+
+        -> TYPE* begin1 (pointer to the first element of the first array [ ex: array1[0] ])
+        -> TYPE* end1   (pointer to the first element after the first array that does not belong [ex: array1[n1] ])
+        -> TYPE* begin2 (pointer to the first element of the second array [ ex: array2[0] ])
+        -> TYPE* end2   (pointer to the first element after the second array that does not belong [ex: array2[n2] ])
+        -> TYPE* output (pointer to the first element of the output array. Make sure it's size is not less than both arrays')
+        -> return       (pointer to the first element in "output" that is not on the merge)
+ */
+
+#define DEFINE_MERGEK(TYPE, TYPENAME, COMPARE) \
+    \
+TYPE* mergeK_##TYPENAME(TYPE* begin1, TYPE* end1, TYPE* begin2, TYPE* end2, TYPE* output) { \
+    while (begin1 != end1 && begin2 != end2) { \
+        if (COMPARE(*begin1, *begin2)) { \
+            *output = *begin2; \
+            ++begin2; \
+        } else { \
+            *output = *begin1; \
+            ++begin1; \
+        } \
+        ++output; \
+    } \
+    while (begin1 != end1) { \
+        *output = *begin1; \
+        ++begin1; \
+        ++output; \
+    } \
+    while (begin2 != end2) { \
+        *output = *begin2; \
+        ++begin2; \
+        ++output; \
+    } \
+    return output; \
+}
+
+#endif
