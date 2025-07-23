c05e868cfdff04052a7a8748c2ac23c7bfc5a0b7
Kikols <francisco.lapamsilva@gmail.com>
2025-07-24 00:05:25 +0100

    "partitionK" first draft

---
+++ src/algorithmsK/partitioningK/partitionK.h
@@ -0,0 +1,51 @@
+#ifndef PARTITIONK_H
+#define PARTITIONK_H
+
+// This is the template macro for the "partitionK_<type>" function
+/*
+    DEFINE_PARTITIONK(TYPE, TYPENAME, FUNC, FUNCNAME)
+        -> TYPE         (type [ ex: int ])
+        -> TYPENAME     (name that created function will take [ ex: int -> partitionK_int_isEven(...) ])
+        -> FUNC         (function that will be used to partition the array [ ex: isEven ])
+        -> FUNCNAME     (name that created function will take [ ex: isEven -> partitionK_int_isEven(...) ])
+
+
+
+
+
+
+    TYPE* partitionK_<type>(TYPE* begin, TYPE* end):
+        Partitions the array in two parts based on the function FUNC.
+        Elements for which FUNC returns true will be in the first part, and the
+        false ones in the second. The pointer to the first element that does not
+        belong to the first part, and the first element of the second part is
+        returned.
+
+
+        -> TYPE* begin  (first element of the first array [ ex: array[0] ])
+        -> TYPE* end    (first element after the first array that does not belong [ex: array[n] ])
+        -> return       (first element of the second part [ ex: array[y] ])
+ */
+
+#define DEFINE_PARTITIONK(TYPE, TYPENAME, FUNC, FUNCNAME) \
+    \
+TYPE* partitionK_##TYPENAME##_##FUNCNAME(TYPE* begin, TYPE* end) { \
+    while (begin != end) { \
+        if (FUNC(*begin)) { \
+            ++begin; \
+        } else { \
+            if (!FUNC(*(end - 1))) { \
+                --end; \
+            } else { \
+                TYPE temp = *begin; \
+                *begin = *(end - 1); \
+                *(end - 1) = temp; \
+                ++begin; \
+                --end; \
+            } \
+        } \
+    } \
+    return begin; \
+}
+
+#endif
