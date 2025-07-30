42bd4744cbf1e039c3654f079619b69673f56632
Kikols <francisco.lapamsilva@gmail.com>
2025-07-30 17:03:41 +0100

    add speed and valid test cases for "partitionpointK"

---
+++ tests/speed_tests/partitionpointK_begin.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "partitioningK.h"
+
+int n;
+
+#define MY_FUNC(e) ((e)<1)
+DEFINE_PARTITIONPOINTK(int, int, MY_FUNC, lesshalf)
+
+int main(int argc, char* argv[]) {
+    n = atoi(argv[1]);
+
+    int* array = (int*)malloc(n*sizeof(int));
+
+    for (int i=0; i<n; ++i) {
+        array[i] = i;
+    }
+
+    clock_t start = clock();
+    for (int i=0; i<100000; ++i) {
+        int* point = partitionpointK_int_lesshalf(array, array+n);
+    }
+    clock_t end = clock();
+    double time = (double)(end-start)/CLOCKS_PER_SEC;
+    printf("%fs", time);
+
+    free(array);
+
+    return 0;
+}
---
+++ tests/valid_tests/partitionpointK_end.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+
+
+int n;
+
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+    n = atoi(argv[1]);
+
+    int* array = (int*)malloc(n*sizeof(int));
+
+    for (int i=0; i<n; ++i) {
+        array[i] = i;
+        cout << array[i] << endl;
+    }
+    cout << endl;
+
+    cout << (partition_point(array, array+n, [](int a) { return (a<n-2); }) - array) << endl << endl;
+
+    for (int i=0; i<n; ++i) {
+        cout << array[i] << endl;
+    }
+
+    free(array);
+    
+    return 0;
+}
